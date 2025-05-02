593485b53908a67146f192e1ad61be9a74eaccbd
CristianIsaacAguilarGuzman <cisaac.aguilar@alumnos.udg.mx>
2025-05-02 09:23:28 -0600

    Correcion de errores y creacion de la primera vista

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,20 +1,19 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>Listado de Eventos</h1>
+<x-app-layout>
+    <div class="container mx-auto py-4">
+        <h1 class="text-2xl font-bold mb-4">Listado de Eventos</h1>
 
         @if ($eventos->isEmpty())
             <p>No hay eventos disponibles.</p>
         @else
-            <ul>
+            <ul class="list-disc pl-6">
                 @foreach ($eventos as $evento)
                     <li>
-                        <a href="{{ route('eventos.show', $evento) }}">{{ $evento->nombre }}</a>
-                        ({{ $evento->fecha }})
+                        <a href="{{ route('eventos.show', $evento) }}" class="text-blue-600 hover:underline">
+                            {{ $evento->nombre }}
+                        </a> ({{ $evento->fecha }})
                     </li>
                 @endforeach
             </ul>
         @endif
     </div>
-@endsection
+</x-app-layout>
